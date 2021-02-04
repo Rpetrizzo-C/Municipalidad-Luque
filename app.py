519d14789d6dfed0aa794b2aae6070b717b64be5
Rodrigo Petrizzo <petrizzo.rodrigoa@gmail.com>
2021-02-04 00:32:04 -0300

    Updates

--- app.py
+++ app.py
@@ -5,9 +5,12 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+   
+@app.route('/contacto')
+def contacto():
+    return render_template('contacto.html')
 
 if __name__ == '__main__':
-    app.run(debug=True)    
-
+    app.run(debug=True)
 
 
